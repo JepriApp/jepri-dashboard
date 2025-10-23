756376aaad4e848d92e34f9c084b8469b733346f
Jutololla <julianangulop@gmail.com>
2025-10-23 09:20:05 -0500

    feat(auth): implement supabase authentication utilities

--- src/services/supabase.client.ts
+++ src/services/supabase.client.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gswoglqqilkhwuedgktp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdzd29nbHFxaWxraHd1ZWRna3RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NjczNTQsImV4cCI6MjA3NTM0MzM1NH0.nwVG6-b810tpqDZri5L-KAouqsBvHw0CBxlnOyt2Oz8';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL or key is not defined');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
